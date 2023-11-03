65741eedc00a33aa5ac69895903623d1a4782f3c
Karanpratap Singh Kharka <129672976+Karanpratap20@users.noreply.github.com>
2023-11-03 23:29:28 +0530

    Time: 90 ms (89.09%) | Memory: 24.4 MB (68.54%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,36 +1,34 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        int n = nums.size();
-        vector<vector<int>> result;
-        
-        for (int i = 0; i < n - 2; i++) {
-            if (i > 0 && nums[i] == nums[i - 1])
-                continue;  
-            
-            int target = -nums[i];
-            int left = i + 1;
-            int right = n - 1;
-            
-            while (left < right) {
-                int sum = nums[left] + nums[right];
-                
-                if (sum < target)
-                    left++;
-                else if (sum > target)
-                    right--;
-                else {
-                    result.push_back({nums[i], nums[left], nums[right]});
-                    while (left < right && nums[left] == nums[left + 1])
-                        left++;  
-                    while (left < right && nums[right] == nums[right - 1])
-                        right--;  
-                    left++;
-                    right--;
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+        vector<vector<int>> v;
+        for(int i=0;i<n-2;i++){
+            if(i>0 && nums[i] == nums[i-1]) continue;
+            int target= -nums[i];
+            int j=i+1;
+            int k=n-1;
+            while(j<k){
+                int sum=nums[j]+nums[k];
+
+                if(target<sum){
+                    k--;
+                }else if(target>sum){
+                    j++;
+                }
+                else{
+                    v.push_back({nums[i],nums[j],nums[k]});
+                    while (j < k && nums[j] == nums[j + 1])
+                        j++;
+                    while (j < k && nums[k] == nums[k - 1])
+                        k--;
+
+                        j++;
+                        k--;
                 }
             }
         }
-        return result;
+        return v;
     }
 };
