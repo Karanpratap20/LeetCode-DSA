dc32218cc78f10dc27c43d002a36eb5ab25f4437
Karanpratap Singh Kharka <129672976+Karanpratap20@users.noreply.github.com>
2023-12-16 13:22:10 +0530

    Time: 3 ms (97.78%) | Memory: 7.8 MB (20.94%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,8 +1,22 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        return s==t;
+        int mp[26]={0};
+        if(s.size()!=t.size()) return false;
+
+        for(int i=0;i<s.size();i++){
+            int it=s[i]-'a';
+            mp[it]++;
+        }
+        for(int i=0;i<s.size();i++){
+            int it=t[i]-'a';
+            mp[it]--;
+        }
+
+        for(int i=0;i<26;i++){
+            if(mp[i]!=0) return false;
+        }
+
+        return true;
     }
 };
