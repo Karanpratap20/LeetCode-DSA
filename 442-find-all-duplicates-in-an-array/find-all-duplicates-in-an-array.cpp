6c409468df86ad2f75be383a479db0512b0cec16
Karan Pratap Singh Kharka <129672976+Karanpratap20@users.noreply.github.com>
2024-08-01 22:34:53 +0530

    Time: 40 ms (63.43%) | Memory: 36.2 MB (68.77%) - LeetSync

--- 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.cpp
+++ 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.cpp
@@ -5,10 +5,12 @@ public:
         for(int i=0;i<nums.size();i++){
             if(nums[abs(nums[i])-1]<0){
                 ans.push_back(abs(nums[i]));
-            }else{
+            }
+            else{
                 nums[abs(nums[i])-1]*=-1;
             }
         }
+
         return ans;
     }
 };
