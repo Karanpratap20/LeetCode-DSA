f341246beb3b981ef346e7dfb0cef74c04b8c8a1
Karan Pratap Singh Kharka <129672976+Karanpratap20@users.noreply.github.com>
2024-08-01 22:51:45 +0530

    Time: 52 ms (92.55%) | Memory: 27.4 MB (69.40%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -3,32 +3,29 @@ public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         int n=nums.size();
         sort(nums.begin(),nums.end());
-        vector<vector<int>> v;
-        for(int i=0;i<n-2;i++){
-            if(i>0 && nums[i] == nums[i-1]) continue;
-            int target= -nums[i];
+        vector<vector<int>> ans;
+
+        for(int i=0;i<n;i++){
+            if(i>0 && nums[i]==nums[i-1]) continue;
+            int target=-nums[i];
             int j=i+1;
             int k=n-1;
             while(j<k){
                 int sum=nums[j]+nums[k];
-
                 if(target<sum){
                     k--;
                 }else if(target>sum){
                     j++;
-                }
-                else{
-                    v.push_back({nums[i],nums[j],nums[k]});
-                    while (j < k && nums[j] == nums[j + 1])
-                        j++;
-                    while (j < k && nums[k] == nums[k - 1])
-                        k--;
+                }else{
+                    ans.push_back({nums[i],nums[j],nums[k]});
+                    while(j<k && nums[j]==nums[j+1])j++;
+                    while(j<k && nums[k]==nums[k-1])k--;
 
-                        j++;
-                        k--;
+                    j++;
+                    k--;
                 }
-            }
+            } 
         }
-        return v;
+        return ans;
     }
 };
