e3cae9fe36ca448a606e681291b29dafb2c44ebf
Karan Pratap Singh Kharka <129672976+Karanpratap20@users.noreply.github.com>
2025-01-24 00:36:45 +0530

    Time: 3 ms (78.26%) | Memory: 17 MB (56.64%) - LeetSync

---
+++ 179-largest-number/largest-number.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    static bool compare(const string& a,const string& b){
+        return a+b>b+a;
+    }
+    string largestNumber(vector<int>& nums) {
+        vector<string> str;
+        for(int it:nums){
+            str.push_back(to_string(it));
+        }
+
+        sort(str.begin(),str.end(),compare);
+        
+        string ans="";
+
+        for(string it:str){
+            ans+=it;
+        }
+
+        if(ans[0]=='0') return "0";
+        return ans;
+    }
+};
