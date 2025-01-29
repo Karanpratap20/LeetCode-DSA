760a58f60da7e9e8b9ea9745cb2e616824ebbd36
Karan Pratap Singh Kharka <129672976+Karanpratap20@users.noreply.github.com>
2025-01-29 13:51:12 +0530

    Time: 8 ms (15.35%) | Memory: 15.6 MB (11.29%) - LeetSync

--- 684-redundant-connection/redundant-connection.cpp
+++ 684-redundant-connection/redundant-connection.cpp
@@ -1,24 +1,53 @@
 class Solution {
 public:
-    int find(vector<int>& dsuf,int v){
-        if(dsuf[v]==-1) return v;
+    bool bfs(int n,vector<unordered_set<int>>& adj,int start){
+        vector<bool> visited(n+1);
+        int count=0;
+        queue<int> q;
+        q.push(start);
+        visited[start]=true;
+        count++;
 
-        return dsuf[v] = find(dsuf,dsuf[v]);
+        while(!q.empty()){
+            int curr=q.front();
+            q.pop();
+
+            for(auto it: adj[curr]){
+                if(!visited[it]){
+                    q.push(it);
+                    count++;
+                    visited[it]=true;
+                }
+            }
+        }
+
+        return count==n;
     }
     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
         int n=edges.size();
 
-        vector<int> dsuf(n+1,-1);
+        vector<unordered_set<int>> adj(n+1);
 
-        for(int i=0;i<n;i++){
-            int px=find(dsuf,edges[i][0]);
-            int py=find(dsuf,edges[i][1]);
-
-            if(px==py) return edges[i];
-
-            dsuf[px]=py;
+        for(auto it:edges){
+            adj[it[0]].insert(it[1]);
+            adj[it[1]].insert(it[0]);
         }
 
+        for(int i=n-1;i>=0;i--){
+            int u=edges[i][0];
+            int v=edges[i][1];
+
+            adj[v].erase(u);
+            adj[u].erase(v);
+
+            if(bfs(n,adj,1)){
+                return {u,v};
+            }
+
+            adj[u].insert(v);
+            adj[v].insert(u);
+        }
+        
         return {0,0};
     }
 };
