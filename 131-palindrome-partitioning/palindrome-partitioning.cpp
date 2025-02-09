fb7dcd1116dcaba91fd9db97ab374cd78f33a84c
Karan Pratap Singh Kharka <129672976+Karanpratap20@users.noreply.github.com>
2025-02-09 15:12:03 +0530

    Time: 57 ms (47.18%) | Memory: 80 MB (24.21%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -1,32 +1,40 @@
 class Solution {
 public:
-    bool isPalindrome(string s,int start,int end){
-        while(start<=end){
-            if(s[start]!=s[end]) return false;
+    vector<vector<string>> res;
+    vector<string> path;
+    vector<vector<int>> dp; // DP table for memoization
+
+    bool isPalindrome(string &s, int start, int end) {
+        while (start <= end) {
+            if (s[start] != s[end]) return false;
             start++;
             end--;
         }
         return true;
     }
-    void func(int ind,string s,vector<string>& path,vector<vector<string>>& res){
-        if(ind==s.size()){
+
+    void func(int ind, string &s) {
+        if (ind == s.size()) {
             res.push_back(path);
             return;
         }
 
-        for(int i=ind;i<s.size();i++){
-            if(isPalindrome(s,ind,i)){
-                path.push_back(s.substr(ind,i-ind+1));
-                func(i+1,s,path,res);
+        for (int i = ind; i < s.size(); i++) {
+            if (dp[ind][i] == -1) {
+                dp[ind][i] = isPalindrome(s, ind, i) ? 1 : 0;
+            }
+            if (dp[ind][i] == 1) {
+                path.push_back(s.substr(ind, i - ind + 1));
+                func(i + 1, s);
                 path.pop_back();
             }
         }
     }
-    vector<vector<string>> partition(string s) {
-        vector<vector<string>> res;
-        vector<string> path;
 
-        func(0,s,path,res);
+    vector<vector<string>> partition(string s) {
+        int n = s.size();
+        dp.assign(n, vector<int>(n, -1)); // Initialize DP table with -1
+        func(0, s);
         return res;
     }
 };
