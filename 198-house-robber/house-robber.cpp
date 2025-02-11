3958e72d129c5a41d33fe762388e1ac6ccc05444
Karan Pratap Singh Kharka <129672976+Karanpratap20@users.noreply.github.com>
2025-02-11 14:27:15 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (21.95%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,13 +1,19 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        int s1=0,s2=0;
-        for(int i=0;i<nums.size();i++){
-            int ns1=s2+nums[i];
-            int ns2=max(s2,s1);
-            s1=ns1;
-            s2=ns2;
+        int n=nums.size();
+        vector<int> dp(n,0);
+
+        dp[0]=nums[0];
+
+        for(int i=1;i<n;i++){
+            int a=nums[i];
+            if(i>1) a+=dp[i-2];
+            int b=dp[i-1];
+
+            dp[i]=max(a,b);
         }
-        return max(s1,s2);
+
+        return dp[n-1];
     }
 };
